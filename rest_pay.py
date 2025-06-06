ddce13cd37024fef8b6e2f5e02145598ffaf1e63
wildhealer <alukichev42@gmail.com>
2025-06-06 19:40:03 +0300

    Update rest_pay.py

--- rest_pay.py
+++ rest_pay.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from gspread-dataframe import set_with_dataframe
+from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 
 
